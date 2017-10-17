05a97c1ad37f2bee360f661657bb16bf276396c6
Mohammed Chlieh <mohammed.chlieh@gmail.com>
2017-10-17 22:09:27 +0200

    fixing tests

--- hwk1/test/test_generative_model.py
+++ hwk1/test/test_generative_model.py
@@ -4,7 +4,7 @@ Unittests for development purposes
 import numpy as np
 import unittest
 
-import hwk1.generative_model as gm
+from hwk1.lda import LDA
 
 
 class TestMleEstimates(unittest.TestCase):
@@ -12,13 +12,13 @@ class TestMleEstimates(unittest.TestCase):
     def test_mle_estimates(self):
         data_x = np.array([[1., 2.], [3., 4.], [2., 3.], [-1., 1.], [-3., 1.], [-3., 3.]])
         data_y = np.array([[1.], [1.], [1.], [0.], [0.], [0.]])
-        mle_estimates = gm.get_mle_estimates(data_x, data_y)
-        self.assertEqual(mle_estimates[0], 0.5, "Pi estimate is wrong. Found %s" % mle_estimates[0])
-        self.assertEqual(mle_estimates[1][0].all(),
+        lda = LDA(data_x, data_y)
+        lda.train()
+        self.assertEqual(lda.pi, 0.5, "Pi estimate is wrong. Found %s" % lda.pi)
+        self.assertEqual(lda.mu_0.all(),
                          np.array([[7./3., 5./3.]]).all(),
-                         "mu_0 estimate is wrong. Found %s" % mle_estimates[1][0])
-        print mle_estimates[2]
-        self.assertEqual(mle_estimates[1][1].all(),
+                         "mu_0 estimate is wrong. Found %s" % lda.mu_0)
+        self.assertEqual(lda.mu_1.all(),
                          np.array([[2., 3.]]).all(),
-                         "mu_1 estimate is wrong. Found %s" % mle_estimates[1][1])
+                         "mu_1 estimate is wrong. Found %s" % lda.mu_1)
 
